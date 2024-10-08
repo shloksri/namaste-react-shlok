e084e7a9d0a5e9c03fec02c60ed7df889f568f05
Shlok <shloksrivastava7@gmail.com>
2024-10-08 18:16:31 +0530

    lecture 2

---
+++ app.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child1" }, [
+        React.createElement("h1", {}, "I am h1 of child1"),
+        React.createElement("h2", {}, "I am h2 of child1"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", {}, "I am h1 of child2"),
+        React.createElement("h2", {}, "I am h2 of child2")
+    ])
+])
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent)
