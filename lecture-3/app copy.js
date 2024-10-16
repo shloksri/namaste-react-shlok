74b54a5f77d9f0e8ae3de1d922b80418a64658a3
Shlok <shloksrivastava7@gmail.com>
2024-10-16 17:05:23 +0530

    lecture-3

---
+++ lecture-3/app copy.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+// React Element
+const heading = (
+    <h1>
+        This is a React Element
+    </h1>
+);
+
+//React Component
+const HeadingComponent = () => {
+    return <h1>
+        This is a React Component.
+    </h1>
+};
+
+
+const Title = () => (
+    <h1>
+        This is title.
+    </h1>
+);
+
+// Component Composition
+const HeadingComponent2 = () => (
+    <div>
+        <h1>Hello</h1>
+        {Title()}
+        <h1> This is Heading Component 2</h1>
+    </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<HeadingComponent2 />)
