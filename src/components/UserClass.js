0b1294d9775fcfdb17d76359146609b30b67c254
Shlok <shloksrivastava7@gmail.com>
2025-01-09 19:18:17 +0530

    ep8 part3

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -6,23 +6,32 @@ class UserClass extends React.Component {
     console.log(props);
     this.state = {
       count: 1,
+      name: "Shlok",
     };
   }
 
   render() {
     const { name, location } = this.props;
-
+    const { count } = this.state; //destructuring state variables
     return (
       <div className="user-card">
-        <h1>Count = {this.state.count}</h1>
+        <h1>Class based component</h1>
+        <h3>Count = {count}</h3>
+
         <button
           onClick={() => {
+            this.state.count = this.state.count + 1;
+          }}>
+          Count Increase
+        </button>
+        {/* <button
+          onClick={() => {
             this.setState({
-              count: this.state.count + 1,
+              count: count + 1,
             });
           }}>
-          Change value
-        </button>
+          Count Increase
+        </button> */}
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h3>Contact: @shloksri</h3>
