87fd20550ecf1e92a69000646d0707dd7b8b5438
Shlok <shloksrivastava7@gmail.com>
2025-01-14 22:36:50 +0530

    ep9 lazy suspense

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import About from "./components/About";
+// import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
+// import Grocery from "./components/Grocery";
+
+const Grocery = lazy(() => import("./components/Grocery"));
+const About = lazy(() => import("./components/About"));
+
 const AppLayout = () => {
   return (
     <div className="app">
@@ -27,7 +32,11 @@ const appRouter = createBrowserRouter([
       },
       {
         path: "/about",
-        element: <About />,
+        element: (
+          <Suspense>
+            <About />
+          </Suspense>
+        ),
       },
       {
         path: "/contact",
@@ -36,6 +45,14 @@ const appRouter = createBrowserRouter([
       {
         path: "/restaurants/:resId",
         element: <RestaurantMenu />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense>
+            <Grocery />
+          </Suspense>
+        ),
       },
     ],
     errorElement: <Error />,
---
+++ src/components/Grocery.js
@@ -0,0 +1,7 @@
+const Grocery = () => {
+  return (
+    <h1>This is a grocery component. We have so much internal components.</h1>
+  );
+};
+
+export default Grocery;
